5d41eb6ffd8f68cc0f92df25756dd6db3a7ae8f3
Manabu Kikuchi <16217397+kerobot@users.noreply.github.com>
2019-01-21 17:02:31 +0900

    顔判定ロジックの修正

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # img_decide
+
+Decide The Image By Model
+
+学習モデルをもとに画像ファイルから特定の顔を認識します。
+
+事前に OpenCV の haarcascade_frontalface_default.xml を用意しておくこと。
+
+## 環境
+
+* Windows 10 x64 1809
+* Python 3.6.5 x64
+* Power Shell 6 x64
+* Visual Studio Code x64
+* Git for Windows x64
+
+## 構築
+
+プロジェクトを clone してディレクトリに移動します。
+
+```powershell
+> git clone https://github.com/kerobot/img_decide.git img_decide
+> cd img_decide
+```
+
+プロジェクトのための仮想環境を作成して有効化します。
+
+```powershell
+> python -m venv venv
+> .\venv\Scripts\activate.ps1
+```
+
+念のため、仮想環境の pip をアップグレードします。
+
+```powershell
+> python -m pip install --upgrade pip
+```
+
+依存するパッケージをインストールします。
+
+```powershell
+> pip install -r requirements.txt
+```
+
+## 実行
+
+modelディレクトリにモデルファイルを配置し、判定する画像ファイルを指定して実行します。
+
+> 判定結果の表示
+
+```powershell
+> python .\img_decide.py "./origin_image/画像ファイル.jpg"
+```
--- img_decide.py
+++ img_decide.py
@@ -1,57 +1,103 @@
+import sys
 import os
 import cv2
 import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-def detect_face(image):
-    print(image.shape)
-    #opencvを使って顔抽出
-    image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cascade = cv2.CascadeClassifier("C:\\Develop\\Bin\\opencv344\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml")
-
+def detect_face(model, image):
+    # 画像をBGR形式からRGB形式へ変換
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # plt.imshow(image)
+    # plt.show()
+    # print(image.shape)
+    # グレースケール画像へ変換
+    image_gs = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     # 顔認識の実行
-    face_list=cascade.detectMultiScale(image_gs, scaleFactor=1.1, minNeighbors=2,minSize=(64,64))
-    #顔が１つ以上検出された時
+    cascade = cv2.CascadeClassifier(CASCADE_FILE_PATH)
+    face_list = cascade.detectMultiScale(image_gs, scaleFactor=1.1, minNeighbors=2, minSize=(64,64))
+
+    # 顔が１つ以上検出できた場合
     if len(face_list) > 0:
+        print(f"認識した顔の数:{len(face_list)}")
         for rect in face_list:
-            x,y,width,height=rect
-            cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 0, 0), thickness=3)
-            img = image[rect[1]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]]
-            if image.shape[0] < 64:
-                print("too small")
+            # 認識した顔の座標とサイズ
+            x, y, width, height = rect
+            # 認識した顔の切り抜き
+            face_image = image[y:y+height, x:x+width]
+            print(f"認識した顔のサイズ:{face_image.shape}")
+            if face_image.shape[0] < 64 or face_image.shape[1] < 64:
+                print("認識した顔のサイズが小さすぎます。")
                 continue
-            img = cv2.resize(image,(64,64))
-            img=np.expand_dims(img,axis=0)
-            name = detect_who(img)
-            cv2.putText(image,name,(x,y+height+20),cv2.FONT_HERSHEY_DUPLEX,1,(255,0,0),2)
+            # 認識した顔のサイズ縮小
+            face_image = cv2.resize(face_image,(64,64))
+            # plt.imshow(face_image)
+            # plt.show()
+            # print(face_image.shape)
+            # 認識した顔のまわりを赤枠で囲む
+            cv2.rectangle(image, (x, y), (x+width, y+height), (255, 0, 0), thickness=2)
+            # 認識した顔を1枚の画像を含む配列に変換
+            face_image = np.expand_dims(face_image, axis=0)
+            # 認識した顔から名前を特定
+            name = detect_who(model, face_image)
+            # 認識した顔に名前を描画
+            cv2.putText(image, name, (x, y+height+20), cv2.FONT_HERSHEY_DUPLEX, 1, (255,0,0) , 2)
     #顔が検出されなかった時
     else:
-        print("no face")
+        print(f"顔を認識できません。")
     return image
 
-def detect_who(img):
-    #予測
-    name=""
-    print(model.predict(img))
-    nameNumLabel=np.argmax(model.predict(img))
-    if nameNumLabel == 0:
-        name = "Honda"
-    elif nameNumLabel == 1:
-        name = "Sakura"
+def detect_who(model, face_image):
+    # 予測
+    name = ""
+    result = model.predict(face_image)
+    print(f"本田 翼　 の可能性:{result[0][0]*100:.3f}%")
+    print(f"佐倉 綾音 の可能性:{result[0][1]*100:.3f}%")
+    name_number_label = np.argmax(result)
+    if name_number_label == 0:
+        name = "Honda Tsubasa"
+    elif name_number_label == 1:
+        name = "Sakura Ayane"
     return name
 
-model = keras.models.load_model("./model.h5")
-image = cv2.imread("./origin_image/01_043.jpg")
+RETURN_SUCCESS = 0
+RETURN_FAILURE = -1
+# Inpou Model Directory
+INPUT_MODEL_PATH = "./model/model.h5"
+# OpenCV Cascades File
+CASCADE_FILE_PATH = "D:\\Develop\\Bin\\opencv344\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml"
 
-if image is None:
-    print("Not open:")
+def main():
+    print("===================================================================")
+    print("顔認識 Keras 利用版")
+    print("学習モデルと指定した画像ファイルをもとに顔を認識して特定します。")
+    print("===================================================================")
 
-b,g,r = cv2.split(image)
-image = cv2.merge([r,g,b])
-whoImage = detect_face(image)
+    # 引数のチェック
+    argvs = sys.argv
+    if len(argvs) != 2 or os.path.exists(argvs[1]) == False:
+        print("画像ファイルを指定して下さい。")
+        return RETURN_FAILURE
+    image_file_path = argvs[1]
 
-plt.imshow(whoImage)
-plt.show()
+    # 画像ファイルの読み込み
+    image = cv2.imread(image_file_path)
+    if image is None:
+        print(f"画像ファイルを読み込めません。{image_file_path}")
+        return RETURN_FAILURE
 
-print("END")
+    # モデルファイルの読み込み
+    if os.path.exists(INPUT_MODEL_PATH) == False:
+        print("モデルファイルが存在しません。")
+        return RETURN_FAILURE
+    model = keras.models.load_model(INPUT_MODEL_PATH)
+
+    # 顔認識
+    result_image = detect_face(model, image)
+    plt.imshow(result_image)
+    plt.show()
+
+    return RETURN_SUCCESS
+
+if __name__ == "__main__":
+    main()
